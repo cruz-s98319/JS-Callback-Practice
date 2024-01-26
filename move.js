482fa53fec5d6f98f1f0a3f697a2c66c847b3148
Samantha Cruz <cruz.s98319@gmail.com>
2024-01-25 21:41:09 -0800

    Fixed bug in move.js causing character to not move

--- move.js
+++ move.js
@@ -13,50 +13,51 @@ function move(element) {
 
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
-    }
+
     
-    function moveCharacter(){
-        if(direction === 'west'){
-            x-=1
-        }
-        if(direction === 'north'){
-            y+=1
-        }
-        if(direction === 'east'){
-            x+=1
-        }
-        if(direction === 'south'){
-            y-=1
+        function moveCharacter(){
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
         }
 
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+        })
+
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
     }
-
-    setInterval(moveCharacter, 1)
-
-    document.addEventListener('keydown', function(e){
-        if(e.repeat) return;
-
-        if(e.key === 'ArrowLeft'){
-            direction = 'west'
-        }
-        if(e.key === 'ArrowUp'){
-            direction = 'north'
-        }
-        if(e.key === 'ArrowRight'){
-            direction = 'east'
-        }
-        if(e.key === 'ArrowDown'){
-            direction = 'south'
-        }
-        callback(direction)
-    })
-
-    document.addEventListener('keyup', function(e){
-        direction = null
-        callback(direction)
-    })
     
     return {
         to: moveToCoordinates,
